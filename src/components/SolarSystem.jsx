8cb1e85f64f7469c711c6d88cc1fa6eb97be4386
Paulo Porto <prtpj1@gmail.com>
2022-02-19 18:32:19 -0300

    Fixed lint errors (I hope) 2

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import Title from './Title';
-import PlanetCard from './PlanetCard';
+// import PlanetCard from './PlanetCard';
 // import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
-    const { planets } = this.props;
+    // const { planets } = this.props;
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
@@ -18,8 +18,8 @@ class SolarSystem extends React.Component {
   }
 }
 
-SolarSystem.propTypes = {
-  planets: PropTypes.string.isRequired,
-};
+// SolarSystem.propTypes = {
+//   planets: PropTypes.string.isRequired,
+// };
 
 export default SolarSystem;
