2fc6da86e12ded30aa0ba6c0b7da511e23598fe3
Paulo Porto <prtpj1@gmail.com>
2022-02-20 18:54:33 -0300

    :yum: feat:(Requisite 10) - Render a list with all missions

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import Title from './Title';
+import MissionsCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <div>
+          {missions.map((elemMission) => (
+            <MissionsCard
+              key={ elemMission.name }
+              name={ elemMission.name }
+              year={ elemMission.year }
+              country={ elemMission.country }
+              destination={ elemMission.destination }
+            />
+          ))}
+        </div>
       </div>
     );
   }
