aa69d449f1c81b49dc08280d8d21417df4e14be7
Paulo Porto <prtpj1@gmail.com>
2022-02-19 22:18:04 -0300

    Req 6 done

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 import Title from './Title';
-// import PlanetCard from './PlanetCard';
-// import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
-    // const { planets } = this.props;
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
-        {/* {planets.map((elemPlanet) => (
-          <PlanetCard key={ elemPlanet.name } planetImg={ elemPlanet.image } />
-        ))} */}
+        {planets.map((elemPlanet) => (
+          <PlanetCard
+            key={ elemPlanet.name }
+            planetName={ elemPlanet.name }
+            planetImage={ elemPlanet.image }
+          />
+        ))}
       </div>
     );
   }
 }
-
-// SolarSystem.propTypes = {
-//   planets: PropTypes.string.isRequired,
-// };
 
 export default SolarSystem;
